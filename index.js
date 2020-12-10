3dd5ffc920eff4e35b8fb22dd714c8e2f01a043b
Brach Sebastien <sebastien.brach@etu.umontpellier.fr>
2020-12-10 10:40:56 +0100

    rien

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000// this is very important
 
 var restdb = {
     method: 'GET',
-    url: 'https://brachnode-dc82.restdb.io/',
+    url: 'https://brachnode-dc82.restdb.io/home/db/5fd1e122dcc309530002b04d/cards/5fd1e252dcc309530002b11b',
     headers: 
     {   'cache-control': 'no-cache',
         'x-apikey': '29a59cfcac6ee5b48b1cec695706df5edabce' 
@@ -14,8 +14,9 @@ var restdb = {
 };
 
 app.get('/', function (req, res) {
-  const testAffichage = axios.get(restdb)
-  res.send(testAffichage)
+  const yes = axios.get(restdb)
+  const print = axios.get('https://brachnode-dc82.restdb.io/rest/Test1?q={"nom": "BRACH"}')
+  res.json(print)
 })
 
 app.listen(PORT, function () {

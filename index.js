6f4f8e20924c9997218f5c3194551ab5ee11db19
Brach Sebastien <sebastien.brach@etu.umontpellier.fr>
2020-12-10 10:20:10 +0100

    ajout api key

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express')
+const axios = require('axios')
 const app = express()
 const PORT = process.env.PORT || 3000// this is very important
+
+
+var restdb = {
+    method: 'GET',
+    url: 'https://brachnode-dc82.restdb.io/',
+    headers: 
+    {   'cache-control': 'no-cache',
+        'x-apikey': '29a59cfcac6ee5b48b1cec695706df5edabce' 
+    } 
+};
 
 app.get('/', function (req, res) {
   res.send('Hello World!')

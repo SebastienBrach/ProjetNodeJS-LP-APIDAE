58ec57972438451739ac05921a6ef11366ff5e8b
diogo-crds <75778702+diogo-crds@users.noreply.github.com>
2021-03-30 16:17:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -109,8 +109,9 @@ app.put("/article/:id", urlEncodedParser, async function (req, res) {
 
 // ne fonctionne pas mais aucune idée pourquoi => code 500 lors du axios.post
 app.post("/register", async function (req, res) {
-  const addUser = await user.createAccount(req.body.mail, req.body.password);
-  res.json({register : addUser});
+  //const addUser = await user.createAccount(req.body.mail, req.body.password);
+  //res.json({register : addUser});
+  res.json(req.body)
 });
 
 app.post("/login", async function (req, res) {

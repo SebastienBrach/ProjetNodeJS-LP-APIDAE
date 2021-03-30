2c74fc2d9daa2c31ff06334fd0d486881c243f03
diogo-crds <75778702+diogo-crds@users.noreply.github.com>
2021-03-30 16:50:40 +0200

    Update user.js

--- user.js
+++ user.js
@@ -11,15 +11,21 @@ const headers = {
 async function createAccount(email, pass) {
     /*if (!email || !password) {
         return({ error: 'Complétez tout les champs' })
-    }*/
+    }
     config = {
         headers : headers
+    }*/
+    const url = 'https://brachnode-dc82.restdb.io/rest/member'
+    const config = {
+        headers: {
+            'x-apikey': '29a59cfcac6ee5b48b1cec695706df5edabce'
+        }
     }
     const data = {
         mail : email, 
         password : pass
     }
-    const response = await axios.post(DBurl, data, config)
+    const response = await axios.post(url, data, config)
     //const addUser = await axios.post(DBurl, data, config)
     //console.log(addUser)
     //return(`User created : ${addUser}`)
